cf7c395da065407f2b6ed4f44c9792d72dde6715
devon-sulistio <devonstinson21japan@gmail.com>
2024-04-07 13:52:09 +0900

    Lesson 168

---
+++ Source/SimpleShooter/GunActor.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GunActor.h"
+#include "Components/SkeletalMeshComponent.h"
+
+// Sets default values
+AGunActor::AGunActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(Root);
+
+	GunMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("GunMesh"));
+	GunMesh->SetupAttachment(Root);
+}
+
+// Called when the game starts or when spawned
+void AGunActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGunActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
